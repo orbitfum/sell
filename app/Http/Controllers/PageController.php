c1ba247a34877b0cf50c68b4cf785ec0847d4cfd
orbitfum <orbitfum@gmail.com>
2017-07-25 00:31:08 +0300

    first commit

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -77,9 +77,9 @@ class PageController extends MainController
 
         $newStr = str_ireplace('width=', "width='100%' ", $str);
         $newStr .= '<div id="google_translate_element"></div><script type="text/javascript">function googleTranslateElementInit() {  new google.translate.TranslateElement({pageLanguage: \'en\', includedLanguages: \'iw\', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, \'google_translate_element\');}</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>';
+        $afterStr = str_ireplace('http://www.ebay.com/itm/', url('ebay/'), $newStr);
 
-
-        self::$data['ifram'] = $newStr;
+        self::$data['ifram'] = $afterStr;
 //        dd(self::$data['ifram'] =$newStr);
 
 
