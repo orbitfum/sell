27146661aa480a49ceae0a47dfca549749a68ecb
bs1601 <bs1601@gmail.com>
2017-07-25 00:41:11 +0300

    תרגום למערכת

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -77,7 +77,7 @@ class PageController extends MainController
 
         $newStr = str_ireplace('width=', "width='100%' ", $str);
         $newStr .= '<div id="google_translate_element"></div><script type="text/javascript">function googleTranslateElementInit() {  new google.translate.TranslateElement({pageLanguage: \'en\', includedLanguages: \'iw\', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, \'google_translate_element\');}</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>';
-        $afterStr = str_ireplace('http://www.ebay.com/itm/', url('ebay/'), $newStr);
+        $afterStr = str_ireplace('http://www.ebay.com/itm/', url('ebay').'/', $newStr);
 
         self::$data['ifram'] = $afterStr;
 //        dd(self::$data['ifram'] =$newStr);
